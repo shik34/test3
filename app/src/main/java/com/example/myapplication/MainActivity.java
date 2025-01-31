fcf833cf2edfdd2ecb261f5ff4f27fb187815e1b
shik34 <shik34@yandex.ru>
2025-01-31 18:41:52 +0700

    3

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void b(View v){
+
+
+        Intent i = new Intent(MainActivity.this, MainActivity2.class);
+        String eText = "information to send";
+        i.putExtra("et", eText);
+        startActivity(i);
+    }
+    @Override
+    protected void onPause(){
+        super.onPause();
+        Toast.makeText(this, "aafdsgsfgfg", Toast.LENGTH_SHORT).show();
+        // Приостановить обновления UI, потоки или трудоемкие процессы,
+        // ненужные, когда активность не на переднем плане
     }
 }
